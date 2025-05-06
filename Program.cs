5cb93816ce16df0ce24030aa66d51abcb491d3b8
Quincy <qsyhuang@gmail.com>
2025-05-06 14:40:28 -0700

    change int ascend/reverse to void

--- Program.cs
+++ Program.cs
@@ -6,16 +6,15 @@ Console.Write("Enter number to print in ascending order: ");
 int number = int.Parse(Console.ReadLine());
 Ascend(number);
 
-int Ascend(int n)
+void Ascend(int n)
 {
     if (n <= 0)
-        return n;
-    
+        return;
+
     Ascend(n - 1);
 
     Console.WriteLine(n);
 
-    return n;
 
 }
 
@@ -23,11 +22,10 @@ Console.Write("Enter number to print in reverse: ");
 int number1 = int.Parse(Console.ReadLine());
 Reverse(number1);
 
-int Reverse(int n)
+void Reverse(int n)
 {
     if (n <= 0)
-        return n;
-
+        return ;
     Console.WriteLine(n);
-    return Reverse(n - 1);
+    Reverse(n - 1);
 }
