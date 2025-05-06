a1ebf80f0eed4b490a1176ba70e593dc34505d19
Quincy <qsyhuang@gmail.com>
2025-05-06 11:28:06 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+Console.Write("Enter number to print in ascending order: ");
+int number = int.Parse(Console.ReadLine());
+Ascend(number);
+
+int Ascend(int n)
+{
+    if (n <= 0)
+        return n;
+    
+    Ascend(n - 1);
+
+    Console.WriteLine(n);
+
+    return n;
+
+}
+
+Console.Write("Enter number to print in reverse: ");
+int number1 = int.Parse(Console.ReadLine());
+Reverse(number1);
+
+int Reverse(int n)
+{
+    if (n <= 0)
+        return n;
+
+    Console.WriteLine(n);
+    return Reverse(n - 1);
+}
